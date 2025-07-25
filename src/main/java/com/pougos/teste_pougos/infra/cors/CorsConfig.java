eb08c84a7f27639db13c4ef6986f08568033b2c0
douglas.araujo <araujodouglas2013@gmail.com>
2025-07-25 11:14:14 -0300

    mudanca-no-cors-para-github-pages

--- src/main/java/com/pougos/teste_pougos/infra/cors/CorsConfig.java
+++ src/main/java/com/pougos/teste_pougos/infra/cors/CorsConfig.java
@@ -11,6 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://127.0.0.1:5500")
+                .allowedOrigins("https://douglasakj.github.io")
                 .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS")
                 .allowCredentials(true);
     }
